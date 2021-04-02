933cf784048d29283f99794cfd4d551428457865
serdarayhan <81782444+serdarayhan@users.noreply.github.com>
2021-04-02 13:03:17 +0200

    Update TClass.java

--- src/TClass.java
+++ src/TClass.java
@@ -1,5 +1,7 @@
 public class TClass {
     public static void main(String[] args) {
+               System.out.println("Github  dan merhab!");
+
         System.out.println("Git den merhab!");
     }
 }
